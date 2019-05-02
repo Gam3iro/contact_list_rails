1ee391c7247c64904111cf472a633dd42bd1c955
Tiago Vieira <tiagomiguelvieira19@gmail.com>
2019-05-02 12:01:54 +0100

    Controllers e validações

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      flash[:success] = 'Utilizador criado com cucesso'
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
+  private
+    def user_params
+      params.require(:user).permit(:nome, :email, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :name, presence: true, length: { maximum: 50 }
-  validate :password, length: { maximum: 6}
+  validates :nome, presence: true, length: { maximum: 50 }
+  validates :password, length: { maximum: 6}
   validates :email, presence: true, length: { maximum: 255 },
                                     format: { with: VALID_EMAIL_REGEX },
                                     uniqueness: { case_sensitive: true }
